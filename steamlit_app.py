fa358b829501e045c10a4e40255aaffe2eb5f219
Jason Arikupurathu <38678270+jasonarikupurathu@users.noreply.github.com>
2022-05-02 14:07:29 -0400

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -4,4 +4,4 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text("""Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg""")
+streamlit.text("""Omega 3 & Blueberry Oatmeal \nKale, Spinach & Rocket Smoothie \nHard-Boiled Free-Range Egg""")
