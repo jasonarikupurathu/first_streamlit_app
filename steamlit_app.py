1473cc9e50bbfb75e5fd689010619a034877e844
Jason Arikupurathu <38678270+jasonarikupurathu@users.noreply.github.com>
2022-05-02 14:07:11 -0400

    Update steamlit_app.py
    Change print to text

--- steamlit_app.py
+++ steamlit_app.py
@@ -4,7 +4,4 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-print("""
-Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg
-
-""")
+streamlit.text("""Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg""")
