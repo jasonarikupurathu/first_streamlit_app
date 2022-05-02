86d360c872169e706690449f524be2b8ddeb8d3c
Jason Arikupurathu <38678270+jasonarikupurathu@users.noreply.github.com>
2022-05-02 14:05:28 -0400

    Update steamlit_app.py
    Change title
    Add breakfast menu

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,4 +1,10 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+print("""
+Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg
+
+""")
